ab51746f32bc3f29cffc764d42fbd436d02934a1
vicgoogle <hugooliveirasbarbosa@gmail.com>
2025-01-14 15:24:16 -0300

    wer

--- src/Services/Rent/CreateRentService.ts
+++ src/Services/Rent/CreateRentService.ts
@@ -44,7 +44,7 @@ export default class CreateService {
     }
 
     //    if (findEquipment.isRented) {
-    //      throw new AppError("Equipamento já alugado");aaaaasd
+    //      throw new AppError("Equipamento já alugado");aaaadfgasd
     //    }
 
     findEquipment.isRented = true;
